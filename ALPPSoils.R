62dcf35b010f56b516448437ef87ce8c90f2b9e5
FelipeMonts <frm10@psu.edu>
2018-05-02 15:05:54 -0400

    working on details of code to extract ALPP pdf information more effectively

--- ALPPSoils.R
+++ ALPPSoils.R
@@ -27,9 +27,19 @@ library(httr)
 library(stringr)
 library(stringi)
 
+# packages for extracting pdf data and manipulating data
+library(tabulizer)
+library(dplyr)
+
 
 
 dir.create("../ALPP_PDFs");
+
+######################################################################################################################## 
+# 
+# Download all the pdf files from the https://www.collaborativetesting.com/store/main.aspx?DepartmentId=40
+# 
+######################################################################################################################## 
 
 # for (i in seq(30,34)) {
 #   download.file(paste0('https://www.collaborativetesting.com/assets/news/', i ,'_WebSum.pdf'), destfile= paste0('../ALPP_PDFs/','ALLP', i, '.pdf'), mode = 'wb' )
@@ -38,31 +48,138 @@ dir.create("../ALPP_PDFs");
   
 
 
-library(tabulizer)
-library(dplyr)
-
-
-ALPP.Tables <- extract_tables(paste0('../ALPP_PDFs/','ALLP', 34, '.pdf')) ;
-
+######################################################################################################################## 
 # 
-# for (k in seq(1,length(NAPT.archive.paths))) {
-#   download.file(NAPT.archive.paths[k], destfile = paste0("../NAPT_ARCHIVE_PDFs/pdf_",k,".pdf"), mode='wb')
-#   }
-
-sssss<-ALPP.Tables[grepl('(SubTestCode 189)',ALPP.Tables)][[3]] ;
-
-str_split(sssss[2,1], " ")
-
-str_split(sssss[3,1], " ")
-
-str_split(sssss[4,1], " ")
-
-str(sssss)
+# Extract the information on the tables inside each PDF file
+# 
+########################################################################################################################
 
 
+  ALPP.Tables <- extract_tables(paste0('../ALPP_PDFs/','ALLP', 20, '.pdf'),pages=c(113:115)) ;
+  
+  # 
+  # for (k in seq(1,length(NAPT.archive.paths))) {
+  #   download.file(NAPT.archive.paths[k], destfile = paste0("../NAPT_ARCHIVE_PDFs/pdf_",k,".pdf"), mode='wb')
+  #   }
+  
+  ########## Extract the Sand Data Summary ########################
+  
+  
+  
+  Texture.data.0<-ALPP.Texture
+  
+ sss<-ALPP.Tables[[1]];
+  
+ Median.row<-which(sss[,1] == "Grand Median") ;
+ MAD.row<-which(sss[,1] == "Median Abs Dev") ;
+ 
+ data.sss<-as.numeric(sss[Median.row,which(!is.na(as.numeric(sss[Median.row,])))])
+ 
 
-Sand<-ALPP.Tables[157][[1]]
+   
+   
+  data.sss<-data.frame(sss[2:4,2:4], stringsAsFactors = F) ;
+  
+  data.sss$X4<-str_split(sss[2,1], " ")[[1]][1] ;
+  
+  data.sss$X4[2]<-str_split(sss[3,1], " ")[[1]][3] ;
+  
+  data.sss$X4[3]<-str_split(sss[4,1], " ")[[1]][4] ;
+  
+  
+  
+  
+  data.sss$X5<-str_split(sss[2,1], " ")[[1]][2] ;
+  
+  data.sss$X5[2]<-str_split(sss[3,1], " ")[[1]][4] ;
+  
+  data.sss$X5[3]<-str_split(sss[4,1], " ")[[1]][5] ;
+  
+  
+  Sand.data<-data.frame(t(data.sss)[,1],as.numeric(t(data.sss)[,2]),as.numeric(t(data.sss)[,3]))  ;
+  names(Sand.data)<-c('Sample' , 'Sand_Mean' , 'Sand_MAD') ;
+  
+  
+  ########## Extract the Silt Data Summary ########################
+  
+  
+  
+  sil<-ALPP.Tables[grepl('(SubTestCode 190)',ALPP.Tables)] [[3]] ;
+  
+  data.sil<-data.frame(sil[2:4,3:5], stringsAsFactors = F) ;
+  
+  data.sil$X4<-str_split(sil[2,2], " ")[[1]][1] ;
+  
+  data.sil$X4[2]<-str_split(sil[3,2], " ")[[1]][1] ;
+  
+  data.sil$X4[3]<-str_split(sil[4,2], " ")[[1]][1] ;
+  
+  
+  data.sil$X5<-str_split(sil[2,2], " ")[[1]][2] ;
+  
+  data.sil$X5[2]<-str_split(sil[3,2], " ")[[1]][2] ;
+  
+  data.sil$X5[3]<-str_split(sil[4,2], " ")[[1]][2] ;
+  
+  
+  Silt.data<-data.frame(t(data.sil)[,1],as.numeric(t(data.sil)[,2]),as.numeric(t(data.sil)[,3]))  ;
+  names(Silt.data)<-c('Sample' , 'Silt_Mean' , 'Silt_MAD') ;
+  
+  
+  ########## Extract the Clay Data Summary ########################
+  
+  
+  
+  ccc<-ALPP.Tables[grepl('(SubTestCode 191)',ALPP.Tables)] [[3]] ;
+  
+  data.ccc<-data.frame(ccc[2:4,3:5], stringsAsFactors = F) ;
+  
+  data.ccc$X4<-str_split(ccc[2,2], " ")[[1]][1] ;
+  
+  data.ccc$X4[2]<-str_split(ccc[3,2], " ")[[1]][1] ;
+  
+  data.ccc$X4[3]<-str_split(ccc[4,2], " ")[[1]][1] ;
+  
+  
+  data.ccc$X5<-str_split(ccc[2,2], " ")[[1]][2] ;
+  
+  data.ccc$X5[2]<-str_split(ccc[3,2], " ")[[1]][2] ;
+  
+  data.ccc$X5[3]<-str_split(ccc[4,2], " ")[[1]][2] ;
+  
+  Clay.data<-data.frame(t(data.ccc)[,1],as.numeric(t(data.ccc)[,2]),as.numeric(t(data.ccc)[,3]))  ;
+  
+  names(Clay.data)<-c('Sample' , 'Clay_Mean' , 'Clay_MAD') ;
+  
+  
+  
+  ########### combine the data together ##################
+  
+  Texture.data<-merge(merge(Sand.data, Silt.data),Clay.data) ;
+  
+  Texture.data.0<-Texture.data  ;
+  
+  ALPP.Texture<-rbind(Texture.data.0,Texture.data)  ;
+  
+  
+  
+  rm(Texture.data , Texture.data.0)
+  
+  
+  
+  
+  
+}
 
-str(Sand[[1]])
 
-ALPP.Tables['Sand 2000-50$']
+
+
+
+
+
+#### Check the consistency of the data
+
+Texture.data$SUM<-Texture.data$Sand_Mean + Texture.data$Silt_Mean + Texture.data$Clay_Mean  ;
+
+str(Sand.data)
+
