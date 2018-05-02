3a9ce60a1dc666d462e73da4ba8d1b36a7954243
FelipeMonts <frm10@psu.edu>
2018-05-02 15:51:10 -0400

    ALPP data extraction

--- ALPPSoils.R
+++ ALPPSoils.R
@@ -55,7 +55,7 @@ dir.create("../ALPP_PDFs");
 ########################################################################################################################
 
 
-  ALPP.Tables <- extract_tables(paste0('../ALPP_PDFs/','ALLP', 20, '.pdf'),pages=c(113:115)) ;
+  ALPP.Tables <- extract_tables(paste0('../ALPP_PDFs/','ALLP', 22, '.pdf'),pages=c(125:127)) ;
   
   # 
   # for (k in seq(1,length(NAPT.archive.paths))) {
@@ -65,97 +65,72 @@ dir.create("../ALPP_PDFs");
   ########## Extract the Sand Data Summary ########################
   
   
+Texture.data.0<-ALPP.Texture
   
-  Texture.data.0<-ALPP.Texture
+sss<-ALPP.Tables[[1]];
   
- sss<-ALPP.Tables[[1]];
-  
- Median.row<-which(sss[,1] == "Grand Median") ;
- MAD.row<-which(sss[,1] == "Median Abs Dev") ;
- 
- data.sss<-as.numeric(sss[Median.row,which(!is.na(as.numeric(sss[Median.row,])))])
+Median.row<-which(sss[,1] == "Grand Median") ;
+MAD.row<-which(sss[,1] == "Median Abs Dev") ;
+Sample.ID<-c('SRS1311' , 'SRS1312', 'SRS1313' ,  'SRS1314' , 'SRS1315') ;
  
 
-   
-   
-  data.sss<-data.frame(sss[2:4,2:4], stringsAsFactors = F) ;
+ 
+ 
+ 
+data.sss<-data.frame(as.numeric(sss[Median.row,which(!is.na(as.numeric(sss[Median.row,])))]), as.numeric(sss[MAD.row,which(!is.na(as.numeric(sss[MAD.row,])))]))
+ 
+str(data.sss)
+ 
+data.sss$Sample<-Sample.ID  ;
+ 
+names(data.sss)[1:2]<-c('Sand_Mean','Sand_MAD' ) ;
+ 
+ 
   
-  data.sss$X4<-str_split(sss[2,1], " ")[[1]][1] ;
+########## Extract the Silt Data Summary ########################
   
-  data.sss$X4[2]<-str_split(sss[3,1], " ")[[1]][3] ;
-  
-  data.sss$X4[3]<-str_split(sss[4,1], " ")[[1]][4] ;
-  
-  
-  
-  
-  data.sss$X5<-str_split(sss[2,1], " ")[[1]][2] ;
-  
-  data.sss$X5[2]<-str_split(sss[3,1], " ")[[1]][4] ;
-  
-  data.sss$X5[3]<-str_split(sss[4,1], " ")[[1]][5] ;
-  
-  
-  Sand.data<-data.frame(t(data.sss)[,1],as.numeric(t(data.sss)[,2]),as.numeric(t(data.sss)[,3]))  ;
-  names(Sand.data)<-c('Sample' , 'Sand_Mean' , 'Sand_MAD') ;
-  
-  
-  ########## Extract the Silt Data Summary ########################
-  
-  
-  
-  sil<-ALPP.Tables[grepl('(SubTestCode 190)',ALPP.Tables)] [[3]] ;
-  
-  data.sil<-data.frame(sil[2:4,3:5], stringsAsFactors = F) ;
-  
-  data.sil$X4<-str_split(sil[2,2], " ")[[1]][1] ;
-  
-  data.sil$X4[2]<-str_split(sil[3,2], " ")[[1]][1] ;
-  
-  data.sil$X4[3]<-str_split(sil[4,2], " ")[[1]][1] ;
+lll<-ALPP.Tables[[2]];
+
+Median.row<-which(lll[,1] == "Grand Median") ;
+MAD.row<-which(lll[,1] == "Median Abs Dev") ;
+
+
+
+
+data.lll<-data.frame(as.numeric(lll[Median.row,which(!is.na(as.numeric(lll[Median.row,])))]), as.numeric(lll[MAD.row,which(!is.na(as.numeric(lll[MAD.row,])))]))
+
+str(data.lll)
+
+data.lll$Sample<-Sample.ID  ;
+
+names(data.lll)[1:2]<-c('Silt_Mean' , 'Silt_MAD' ) ;
   
   
-  data.sil$X5<-str_split(sil[2,2], " ")[[1]][2] ;
-  
-  data.sil$X5[2]<-str_split(sil[3,2], " ")[[1]][2] ;
-  
-  data.sil$X5[3]<-str_split(sil[4,2], " ")[[1]][2] ;
-  
-  
-  Silt.data<-data.frame(t(data.sil)[,1],as.numeric(t(data.sil)[,2]),as.numeric(t(data.sil)[,3]))  ;
-  names(Silt.data)<-c('Sample' , 'Silt_Mean' , 'Silt_MAD') ;
-  
+ 
   
   ########## Extract the Clay Data Summary ########################
   
-  
-  
-  ccc<-ALPP.Tables[grepl('(SubTestCode 191)',ALPP.Tables)] [[3]] ;
-  
-  data.ccc<-data.frame(ccc[2:4,3:5], stringsAsFactors = F) ;
-  
-  data.ccc$X4<-str_split(ccc[2,2], " ")[[1]][1] ;
-  
-  data.ccc$X4[2]<-str_split(ccc[3,2], " ")[[1]][1] ;
-  
-  data.ccc$X4[3]<-str_split(ccc[4,2], " ")[[1]][1] ;
-  
-  
-  data.ccc$X5<-str_split(ccc[2,2], " ")[[1]][2] ;
-  
-  data.ccc$X5[2]<-str_split(ccc[3,2], " ")[[1]][2] ;
-  
-  data.ccc$X5[3]<-str_split(ccc[4,2], " ")[[1]][2] ;
-  
-  Clay.data<-data.frame(t(data.ccc)[,1],as.numeric(t(data.ccc)[,2]),as.numeric(t(data.ccc)[,3]))  ;
-  
-  names(Clay.data)<-c('Sample' , 'Clay_Mean' , 'Clay_MAD') ;
-  
-  
-  
+
+ccc<-ALPP.Tables[[3]];
+
+Median.row<-which(ccc[,1] == "Grand Median") ;
+MAD.row<-which(ccc[,1] == "Median Abs Dev") ;
+
+
+
+
+data.ccc<-data.frame(as.numeric(ccc[Median.row,which(!is.na(as.numeric(ccc[Median.row,])))]), as.numeric(ccc[MAD.row,which(!is.na(as.numeric(ccc[MAD.row,])))]))
+
+str(data.ccc)
+
+data.ccc$Sample<-Sample.ID  ;
+
+names(data.ccc)[1:2]<-c('Clay_Mean' , 'Clay_MAD') ;
+
+
   ########### combine the data together ##################
   
-  Texture.data<-merge(merge(Sand.data, Silt.data),Clay.data) ;
+  Texture.data<-merge(merge(data.sss, data.lll),data.ccc) ;
   
   Texture.data.0<-Texture.data  ;
   
@@ -167,9 +142,7 @@ dir.create("../ALPP_PDFs");
   
   
   
-  
-  
-}
+
 
 
 
