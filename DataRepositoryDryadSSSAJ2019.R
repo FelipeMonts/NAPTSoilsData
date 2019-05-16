f6e4f5cb827a5ca40828276e2c46f6835a824b73
FelipeMonts <frm10@psu.edu>
2019-05-16 16:46:53 -0400

    Updated Data Repository

--- DataRepositoryDryadSSSAJ2019.R
+++ DataRepositoryDryadSSSAJ2019.R
@@ -25,7 +25,7 @@
 
 #  Set Working directory
 
-setwd("C:\\Felipe\\LaserDifractionSoilTextureAnalysis\\Manuscript") ;
+setwd("C:\\Felipe\\LaserDifractionSoilTextureAnalysis\\NAPTSoilsData") ;
 
 
 #Loand And install packages 
@@ -42,28 +42,72 @@ library(openxlsx) ;
 
 
 ###############################################################################################################
-#                         Read data sets into R            
+#                         Read Standard data into R and compare them.          
 ###############################################################################################################
 
+StandardDataSSJ<-read.xlsx("Laser and Std Results for SSSAJ.xlsx", sheet="Standard Data", colNames=T, startRow = 2 ) ;
 
-LaserDS.data<-read.xlsx("./Review/Second Review/Laser and Std Results for SSSAJ.xlsx", sheet="Laser Diffraction-Sieving Data", colNames=T) ;
+names(StandardDataSSJ)
+
+names(StandardDataSSJ)<-c("Sample", "P_CLAY",   "P_SILT" ,  "P_SAND" ,  "H_CLAY"  , "H_SILT"  , "H_SAND"  )
+
+Ordered.DataSSJ<-StandardDataSSJ[order(StandardDataSSJ$Sample),]
 
 
-names(LaserDS.data)
+
+
+Table1.names<-read.xlsx("../Manuscript/Table1_20190510.xlsx", sheet="New (5)", colNames=F, startRow = 1, rows=c(1,2) ) ;
+
+as.character(Table1.names[1,])
+
+
+Table1.data<-read.xlsx("../Manuscript/Table1_20190510.xlsx", sheet="New (5)", colNames=F, startRow = 3 ) ;
+
+names(Table1.data)<-as.character(Table1.names[1,])
+
+names(Table1.data)<-c("Sample" , "Location",   "Soil classification" ,"P_Sand",  "P_Silt" , "P_Clay" , "n" ,"H_Sand", "H_Silt" , "H_Clay",                "n" , "TOC " , "CaCO3" )
+
+Ordered.Table1<-Table1.data[order(Table1.data$Sample),]
+
+
+plot(as.numeric(Ordered.DataSSJ$P_CLAY)*100, type="p", pch=21, col="RED", cex=1.5, bg=NA)
+points(Ordered.Table1$P_Clay, type="p" , pch=21, col="BLUE", bg="BLUE")
+
+plot(as.numeric(Ordered.DataSSJ$P_SILT)*100, type="p", pch=21, col="RED", cex=1.5, bg=NA)
+points(Ordered.Table1$P_Silt, type="p" , pch=21, col="BLUE", bg="BLUE")
+
+plot(as.numeric(Ordered.DataSSJ$P_SAND)*100, type="p", pch=21, col="RED", cex=1.5, bg=NA)
+points(Ordered.Table1$P_Sand, type="p" , pch=21, col="BLUE", bg="BLUE")
+
+
+plot(as.numeric(Ordered.DataSSJ$H_CLAY)*100, type="p", pch=21, col="RED", cex=1.5, bg=NA)
+points(Ordered.Table1$H_Clay, type="p" , pch=21, col="BLUE", bg="BLUE")
+
+plot(as.numeric(Ordered.DataSSJ$H_SILT)*100, type="p", pch=21, col="RED", cex=1.5, bg=NA)
+points(Ordered.Table1$H_Silt, type="p" , pch=21, col="BLUE", bg="BLUE")
+
+plot(as.numeric(Ordered.DataSSJ$H_SAND)*100, type="p", pch=21, col="RED", cex=1.5, bg=NA)
+points(Ordered.Table1$H_Sand, type="p" , pch=21, col="BLUE", bg="BLUE")
+
+
+###############################################################################################################
+#                         Read Laser Diff  data into R and compare them.          
+###############################################################################################################
 
 
 
 ############################################## Read the names of the samples from the spreadsheet ######################################
 
-LassDiff.name<-read.xlsx("C:/Felipe/LaserDifractionSoilTextureAnalysis/Manuscript/USDA Standards_PSA_Mastersizer_Felipe_20180824.xlsx", sheet="correct (6) fraction",startRow=2, colNames=F, rows=c(2)) ; 
+LaserSSAJ.data<-read.xlsx("Laser and Std Results for SSSAJ.xlsx", sheet="Laser Diffraction-Sieving Data", colNames=T) ;
 
 
+names(LaserSSAJ.data)
 
 
-#grep("SRS",LassDiff.name)
+LaserSSAJ<-LaserSSAJ.data[,order(names(LaserSSAJ.data))]
 
-############################################# Read the LD data from the spreadsheet  ########################################################
 
+LassDiff.name<-read.xlsx("../Manuscript/USDA Standards_PSA_Mastersizer_Felipe_20180824.xlsx", sheet="correct (6) fraction",startRow=2, colNames=F, rows=c(2)) ; 
 
 
 
@@ -75,33 +119,44 @@ str(LassDiff.1)
 names(LassDiff.1)<-c('Row','Size', 'nothing' ,sapply(strsplit(as.character(LassDiff.name[1,]),"/"),"[", 1))
 
 
-#####################################################  Organizing column names to compare them withw the spreadsheet names ##################
-
 LassDiff.names1<-sapply(strsplit(names(LassDiff.1)[4:54]," "), "[",2) ;
 
 LassDiff.names2<-sapply(strsplit(LassDiff.names1,"-5"),"[",1) ;
 
 
-names(LaserDS.data) %in% LassDiff.names2
-
-LassDiff.names2 %in% names(LaserDS.data)
+names(LassDiff.1)<-c('Row','Size', 'nothing' ,LassDiff.names2)
 
 
-str(names(LaserDS.data))
-str(LassDiff.names2)
-
-names(LaserDS.data)[c(1,21,31,43,46)]
-
-match(names(LaserDS.data)[-1],LassDiff.names2)
-
-match(LassDiff.names2,names(LaserDS.data)[-1])
+names(LassDiff.1[,c(2,4:54)])
 
 
+LassDiff.2<-LassDiff.1[,order(names(LassDiff.1))]
 
-names(LaserDS.data)[!(names(LaserDS.data) %in% LassDiff.names2)]
- 
 
-match(names(LaserDS.data)[!(names(LaserDS.data) %in% LassDiff.names2)], names(LaserDS.data))
+# names(LaserDS.data) %in% LassDiff.names2
+# 
+# LassDiff.names2 %in% names(LaserDS.data)
+# 
+# 
+# str(names(LaserDS.data))
+# str(LassDiff.names2)
+# 
+# names(LaserDS.data)[c(1,21,31,43,46)]
+# 
+# match(names(LaserDS.data)[-1],LassDiff.names2)
+# 
+# match(LassDiff.names2,names(LaserDS.data)[-1])
+# 
+# 
+# 
+# names(LaserDS.data)[!(names(LaserDS.data) %in% LassDiff.names2)]
+#  
+# 
+# match(names(LaserDS.data)[!(names(LaserDS.data) %in% LassDiff.names2)], names(LaserDS.data))
+
+
+
+
 
 
 ##############################################################################################################################################
